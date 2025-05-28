8463c1eef7088567e94015d542e60ef6f2e1366b
franklin-sula <franklin.sula09@gmail.com>
2025-05-28 17:00:54 +0800

    Update consultation button

--- src/components/Consultation/ConsultationButton.jsx
+++ src/components/Consultation/ConsultationButton.jsx
@@ -2,34 +2,51 @@ import PropTypes from "prop-types";
 import { Icon } from "@iconify/react";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
+import { checkConsultationExistence } from "@/services/consultationServices";
+import { useQuery } from "@tanstack/react-query";
+import { useUser } from "@/context/useUser";
 
 const ConsultationButton = ({ isMobile = true }) => {
   const navigate = useNavigate();
+  const { userData } = useUser();
 
   const handleNavigation = () => {
     navigate("/consultation");
   };
 
+  const { data, isFetched } = useQuery({
+    queryKey: ["consultationExistence"],
+    queryFn: async () => await checkConsultationExistence(userData?.id),
+    enabled: !!userData?.id,
+  });
+
+  const showExclamationMark = isFetched && !data?.consultationExist;
+
   if (isMobile) {
     return (
       <div
-        className="mt-2 h-full rounded-full border border-accent/5 bg-primary px-2 py-3 shadow-lg lg:hidden"
+        className="consultation-button mt-2 h-full rounded-full px-[10px] py-[13px] shadow-lg lg:hidden"
         onClick={handleNavigation}
       >
         <div className="flex h-full flex-col items-center justify-end">
-          <Button variant="ghost" className="relative block h-auto p-0">
-            <div className="absolute -right-4 -top-4">
-              <div className="flex h-5 w-5 items-center justify-center rounded-full bg-danger text-[11px] text-white">
-                !
+          <Button
+            variant="ghost"
+            className="relative block h-auto p-0 hover:bg-inherit"
+          >
+            {showExclamationMark && (
+              <div className="absolute -right-4 -top-4">
+                <div className="flex h-5 w-5 items-center justify-center rounded-full bg-danger text-[11px] text-white">
+                  !
+                </div>
               </div>
-            </div>
+            )}
             <Icon
               icon="hugeicons:task-daily-01"
-              className="text-primary-text"
-              fontSize={20}
+              className="text-white"
+              fontSize={28}
             />
           </Button>
-          <p className="mt-2 text-center text-[7px] font-extrabold text-accent">
+          <p className="text-center text-[7px] font-extrabold text-white">
             Consultation
           </p>
         </div>
@@ -45,9 +62,11 @@ const ConsultationButton = ({ isMobile = true }) => {
         <Icon icon="hugeicons:task-daily-01" className="h-6 w-6" />
         <span>Consultation</span>
       </div>
-      <span className="flex h-full w-12 items-center justify-center rounded-full bg-danger text-white">
-        !
-      </span>
+      {showExclamationMark && (
+        <span className="flex h-full w-12 items-center justify-center rounded-full bg-danger text-white">
+          !
+        </span>
+      )}
     </div>
   );
 };
