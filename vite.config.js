85bac7c414947606e795851341f51c7bfeb21c99
Franklin Sula <f.sula@a2kgroup.org>
2025-12-04 14:52:19 +0800

    Add PWA support and update dependencies
    Added PWA icons and integrated vite-plugin-pwa in vite.config.js and package files. Updated Babel dependencies and fixed favicon type in index.html to improve PWA compatibility.

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,45 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    basicSsl(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Saint Laurence",
+        short_name: "Saint Laurence",
+        description: "Parish Management Portal",
+        start_url: "/",
+        display: "standalone",
+        orientation: "portrait",
+        theme_color: "#663E2F",
+        background_color: "#F6F0ED",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), 
+      "@": path.resolve(__dirname, "src"),
     },
+  },
+  server: {
+    host: true,
   },
   // base: "/portal/",
 });
